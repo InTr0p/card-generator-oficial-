1f83c9d5e08c4598fca2ed37dc0fd67282119a94
InTr0p <christianolivare01@gmail.com>
2022-02-10 23:42:44 +0000

    subida final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let generateCard = () => {
+    let card = ["diamond", "heart", "club", "spades"];
+    let i = Math.floor(Math.random() * card.length);
+    return card[i];
+  };
+  let generateNumberCard = () => {
+    let CardNumbers = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q"];
+    let iCardNumbers = Math.floor(Math.random() * CardNumbers.length);
+    return CardNumbers[iCardNumbers];
+  };
+  document.querySelector(".card").className = `card ${generateCard()}`;
+  document.querySelector(".card").innerHTML = generateNumberCard();
+  setInterval(() => {
+    document.querySelector(".card").className = `card ${generateCard()}`;
+    document.querySelector(".card").innerHTML = generateNumberCard();
+  }, 100000);
 };
+let Button = document.querySelector(".button");
+Button.addEventListener("click", () => {
+  window.onload();
+});
